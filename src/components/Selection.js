687ccb4417e14d9847fac6753cfbbbcd65a814ef
abhishek2813 <abhi93158879@gmail.com>
2023-03-14 22:37:29 +0530

    solve

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,10 @@
+import React from 'react'
+
+export default function Selection(props) {
+  return (
+    <div>
+       <div style={props.applyColor} className="fix-box">{props.key}</div>
+    </div>
+  )
+}
 
